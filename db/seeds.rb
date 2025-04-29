72b4f6cbd9fb1c9f04bb72391ff86d1be819b576
Chris Barry <chrisbarryrocks@gmail.com>
2025-04-28 23:16:04 -0400

    More setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Post.create!([
+  { title: "First Post", content: "Lorem ipsum dolor sit amet." },
+  { title: "Second Post", content: "Another exciting update." },
+  { title: "Third Post", content: "Even more blog thoughts." },
+  { title: "Fourth Post", content: "Blogging about blogging." },
+  { title: "Fifth Post", content: "One more for good measure." }
+])
